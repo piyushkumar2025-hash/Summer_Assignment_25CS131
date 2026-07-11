5d7fb0ac2a3588f34a4522973b89bb2f9aad3464
piyushkumar2025-hash <piyush.kumar2025@glbajajgroup.org>
2026-07-11 12:48:21 +0530

    Logical error fixed

--- Day-22/Question87.c
+++ Day-22/Question87.c
@@ -1,20 +1,25 @@
 // Write a program to Character frequency.
 
 #include<stdio.h>
+
 int main() {
-    char str[100],n;
-    int i = 0,frequency = 0;
+    char str[100], n;
+    int i = 0, frequency = 0;
+
     printf("Enter a Sentence: ");
     fgets(str, sizeof(str), stdin);
+
     printf("Enter the character whose frequency need to be checked: ");
-    scanf("%c",&n);
+    scanf(" %c", &n);
+
     while(str[i] != '\0'){
         if(str[i] == n){
             frequency++;
         }
         i++;
     }
-    printf("frequency of %c in the sentence is: %d",n,frequency);
-    
+
+    printf("Frequency of %c in the sentence is: %d", n, frequency);
+
     return 0;
 }
